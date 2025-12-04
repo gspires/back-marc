3935cdd292b39add7475d885a6b19290693b056e
Gutierre <gutierre.pires@gmail.com>
2025-12-04 09:07:39 -0300

    populando user

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -1,12 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 import { IsPublic } from 'src/auth/decorators/is-public.decorator';
 import { CurrentUser } from 'src/auth/decorators/current-user.decorator';
 import { User } from 'src/user/entities/user.entity';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private prisma: PrismaService,
+  ) { }
 
   @IsPublic()
   @Get()
@@ -15,7 +19,27 @@ export class AppController {
   }
 
   @Get('me')
-  getMe(@CurrentUser() user: User){
+  getMe(@CurrentUser() user: User) {
     return user;
+  }
+
+  // 🚀 ENDPOINT DE SEED PARA INSERIR UM USUÁRIO
+  @IsPublic() // opcional
+  @Post('seed-user')
+  async seedUser() {
+    const user = await this.prisma.user.create({
+      data: {
+        name: "admin",
+        email: "admin@gmail.com",
+        password: "123@Mudar",
+        cpf: "12345678989",
+        role: "admin"
+      }
+    })
+
+    return {
+      message: 'Usuário criado com sucesso!',
+      user,
+    };
   }
 }
